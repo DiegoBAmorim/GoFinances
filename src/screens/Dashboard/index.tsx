390584ab6ebb79c7fbfe97246a608be01cebe1f1
Lucas Cavalcante <lucas-cavalcante-2@hotmail.com>
2022-04-11 09:59:28 -0300

    Add Typescript  and create components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { HighlightCard } from "../../components/HighlightCard";
+
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon,
+  HighlightCards,
+} from "./styles";
+
+export function Dashboard() {
+  return (
+    <Container>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: "https://avatars.githubusercontent.com/u/91428845?v=4" }} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Lucas Cavalcante</UserName>
+            </User>
+          </UserInfo>
+          <Icon name="power" />
+        </UserWrapper>
+      </Header>
+      <HighlightCards>
+        <HighlightCard />
+        <HighlightCard />
+        <HighlightCard />
+      </HighlightCards>
+    </Container>
+  );
+}
